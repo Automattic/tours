7bdc6ee06ca1cb7c116ff5a947d5a04dca7d4821
Alex Kirk <akirk@users.noreply.github.com>
2023-10-23 14:49:06 +0200

    typo

--- tour.php
+++ tour.php
@@ -9,8 +9,8 @@
  */
 
 function tour_enqueue_scripts() {
-	wp_register_style( 'driver-js', plugins_url( 'assets/css/driver-js.css', __FILE__ ), array(), filemtime( __DIR__ . 'assets/css/driver-js.css' ) );
-	wp_enqueue_script( 'driver-js', plugins_url( 'assets/js/driver-js.js', __FILE__ ), array(), filemtime( __DIR__ . 'assets/js/driver-js.js' ), array( 'in_footer' => true ) );
+	wp_register_style( 'driver-js', plugins_url( 'assets/css/driver-js.css', __FILE__ ), array(), filemtime( __DIR__ . '/assets/css/driver-js.css' ) );
+	wp_enqueue_script( 'driver-js', plugins_url( 'assets/js/driver-js.js', __FILE__ ), array(), filemtime( __DIR__ . '/assets/js/driver-js.js' ), array( 'in_footer' => true ) );
 }
 
 add_action( 'wp_enqueue_scripts', 'tour_enqueue_scripts' );
