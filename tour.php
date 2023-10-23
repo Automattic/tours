ff731305268b916425c47f6f6ddf6c0830a007ca
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2023-10-23 15:30:43 +0200

    Add the tour.js script and an example tour

--- tour.php
+++ tour.php
@@ -6,14 +6,46 @@
  * Version: 1.0
  * Author: Automattic
  * Author URI: http://automattic.com/
+ * Text Domain: tour
+ * License: GPLv2 or later
  */
 
 function tour_enqueue_scripts() {
 	wp_register_style( 'driver-js', plugins_url( 'assets/css/driver-js.css', __FILE__ ), array(), filemtime( __DIR__ . '/assets/css/driver-js.css' ) );
 	wp_enqueue_script( 'driver-js', plugins_url( 'assets/js/driver-js.js', __FILE__ ), array(), filemtime( __DIR__ . '/assets/js/driver-js.js' ), array( 'in_footer' => true ) );
-}
+	wp_register_script( 'gp-tour', plugins_url( 'assets/js/tour.js', __FILE__ ), array( 'jquery', 'driver-js' ), filemtime( __DIR__ . '/assets/js/tour.js' ), false );
+	wp_set_script_translations( 'gp-tour', 'tour' );
+	wp_localize_script( 'gp-tour', 'gp_tour', apply_filters( 'gp_tour', array() ) );}
 
 add_action( 'wp_enqueue_scripts', 'tour_enqueue_scripts' );
+
+add_filter( 'gp_tour', function( ) {
+	return array(
+		'ui-intro' => [
+			[
+				'title' => 'UI Introduction Tour',
+				'color' => '#3939c7',
+			],
+			[
+				'selector' => '.strings .original',
+				'html' => 'This is the original string that needs translation'
+			],
+			[
+				'selector' => 'textarea.foreign-text',
+				'html' => 'Enter translation here',
+			],
+			[
+				'selector' => '.sidebar-tabs',
+				'html' => 'See meta tabs here',
+			],
+			[
+				'selector' => '.button.ok',
+				'html' => 'Click here to save translation',
+			]
+		],
+
+	);
+} );
 
 add_action( 'wp_footer', function() {
 ?>
