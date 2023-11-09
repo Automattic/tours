e906ad4d9e3695449a127a2a8a79459f41cd6999
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2023-11-09 18:51:24 +0100

    Update the name, the description and the author

--- assets/blocks/build/index.asset.php
+++ assets/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'cf61f320850f0eb04fd1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '329adc86b1b74e3540e6');
