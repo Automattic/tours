61cc52dea6672d4cc8d1fdd031784345d72486bb
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2023-11-13 13:40:58 +0100

    Update the incorrect name (singular)

--- assets/blocks/build/index.asset.php
+++ assets/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '329adc86b1b74e3540e6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '17bcf5206c604e1ad5b0');
