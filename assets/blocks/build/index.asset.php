09a1371eba777e0e9fd0886e4b508ce73308e6ef
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2023-11-13 14:38:16 +0100

    Remove the CSS from the SCSS files. Maintain only the class name

--- assets/blocks/build/index.asset.php
+++ assets/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '8f87d41e197f3a111259');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '5245d674970fcb4f5855');
