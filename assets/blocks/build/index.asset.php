4e949968939ecaa77014aab8aa79b40a8bf8b26d
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2023-11-13 13:48:44 +0100

    Remove the save function

--- assets/blocks/build/index.asset.php
+++ assets/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '17bcf5206c604e1ad5b0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '8f87d41e197f3a111259');
