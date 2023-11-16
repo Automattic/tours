5acf7fa7c0d02889c99e350a0a66a5183e8fe0c7
Alex Kirk <akirk@users.noreply.github.com>
2023-11-16 11:29:46 +0100

    Add some more useful example test

--- assets/blocks/build/index.asset.php
+++ assets/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '29e4fd4445fe59e85eaa');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ff020029dbdcac61b39f');
