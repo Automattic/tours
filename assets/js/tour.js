f2dec35b0292f6d54ab1507bc3e8cd03ff424ba0
Alex Kirk <akirk@users.noreply.github.com>
2023-11-24 13:16:22 +0100

    Don't wait until the server response before the tour is restarted

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -300,22 +300,20 @@ document.addEventListener( 'DOMContentLoaded', function () {
 			xhr.open( 'POST', tour_plugin.rest_url + 'tour/v1/save-progress' );
 			xhr.setRequestHeader( 'Content-Type', 'application/json' );
 			xhr.setRequestHeader( 'X-WP-Nonce', tour_plugin.nonce );
-			xhr.onload = function () {
-				if ( xhr.status >= 200 && xhr.status < 300 ) {
-					delete tour_plugin.progress[ tourId ];
-					loadTour();
-					pulseToClick = document.querySelector(
-						'.pulse.tour-' + tourId
-					);
-					pulseToClick.click();
-				}
-			};
 			xhr.send(
 				JSON.stringify( {
 					tour: tourId,
 					step: -1,
 				} )
 			);
+
+			delete tour_plugin.progress[ tourId ];
+			loadTour();
+			pulseToClick = document.querySelector(
+				'.pulse.tour-' + tourId
+			);
+			pulseToClick.click();
+
 		}
 	} );
 } );
