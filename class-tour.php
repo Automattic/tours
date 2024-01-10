7ae43762717a49cb74932dcf966f6da2d7f97ac6
Tosin Oguntuyi <tcubed03@gmail.com>
2024-01-10 15:20:43 +0100

    Remove unused line

--- class-tour.php
+++ class-tour.php
@@ -59,7 +59,6 @@ class Tour {
 				}
 
 				if ( $tour[0]['tour_restrict_url'] && preg_match( '#' . ( $tour[0]['tour_restrict_url'] ) . '#', wp_parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH ) . '?' . wp_parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY ) ) ) {
-					echo $post_id;
 					return true;
 				}
 
